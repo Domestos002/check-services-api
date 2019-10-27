a81d414b7c871c65beff70049978f8b82f61e965
lamer <absorber000@gmail.com>
2019-10-27 22:23:42 +0600

    change configs

--- src/config/db.php
+++ src/config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=mysql;dbname=services',
     'username' => 'root',
-    'password' => '8JJxJHl!69TEv#D',
+    'password' => '',
     'charset' => 'utf8',
 ];
